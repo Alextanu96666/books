d998fe5d21d65d67b9da3e3e3de420f16389f6fe
Alex Tanu <alex.tanu96@gmail.com>
2019-05-28 10:53:23 +0200

    vendor

---
+++ final_projekt/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" enctype="multipart/form-data" action="second.php">
+    
+    <input type="file" name="theFile" value="Välj fil">
+    <input type="submit" value="send" name="submit">
+</form>
+<pre>
+    <?php
+    
+    
+    
+
+    ?>
+</body>
+</html>
